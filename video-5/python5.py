4add94a07d2f68091f96c84815c7cd5e008ed6e1
Shubhanshu Nishad <69467788+Shubhanshu-Nishad@users.noreply.github.com>
2020-09-19 19:09:55 +0530

    Add files via upload

---
+++ video-5/python5.py
@@ -0,0 +1,12 @@
+print('shubhanshu Nishad')
+#this is a single comments
+"""
+this is a multi lines  comments
+this is a multi lines  comments
+this is a multi lines  comments
+""" 
+# print("hi I'm learning python")
+# print("shubhuji's said \"i am a good boy" )
+# print('shubhuji \n s') # for new lines
+# print('shubhuji \t s') #8 character spaces
+print('this is a multi lines \r commen')
