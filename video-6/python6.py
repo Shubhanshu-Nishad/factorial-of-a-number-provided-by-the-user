e19d1432b22973ccb27b0eca070c2aea5a2b8754
Shubhanshu Nishad <69467788+Shubhanshu-Nishad@users.noreply.github.com>
2020-09-19 21:11:13 +0530

    Add files via upload

---
+++ video-6/python6.py
@@ -0,0 +1,17 @@
+# x=5
+# x='subscribe '
+# y="share it"
+# print(x)
+# print(y)
+meta=56
+META=5
+print(meta)
+print(META)
+_meta=7
+print(_meta)
+# @meta=8  this is not allow (special character)
+# 9meta=7 this is not allow (number)
+
+# role for varibles
+# # must start with alphbet
+# # must start _ underscroe
