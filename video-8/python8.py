b9daa60b8dabaa747021f00297b6ddaadc50f8c6
Shubhanshu Nishad <69467788+Shubhanshu-Nishad@users.noreply.github.com>
2020-09-20 19:04:00 +0530

    Add files via upload

---
+++ video-8/python8.py
@@ -0,0 +1,39 @@
+# Arithmetic
+# x=5+6
+# v=5-2
+# c=9*3
+# d=9/3
+# print(x)
+# print(v)
+# print(c)
+# print(d)
+
+# Assignment operators
+
+# x=5
+# x/=5
+# # x=x/5
+# print(x)
+
+# x=9%3 # return remainder
+# print(x)
+
+# y=9
+# y%=3# return remainder
+# print(y)
+
+# x=2
+# x**=2
+# print(x)
+
+# x=9
+# y=5
+# print(x<=y)
+x=2
+y=45
+print((x<15  or y<15 ))
+
+
+
+
+
