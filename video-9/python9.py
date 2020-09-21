5312ff27afbb35ead6703df8ff8a656ca1bcdeb1
Shubhanshu Nishad <69467788+Shubhanshu-Nishad@users.noreply.github.com>
2020-09-21 09:28:17 +0530

    Add files via upload

---
+++ video-9/python9.py
@@ -0,0 +1,12 @@
+# # x=6
+# # y=float(5)
+# # print(type(x))
+# # print(type(y))
+# x='subscribe'
+# y=float(x)
+# # print(type(x))
+x=int("65")
+print(type(x))
+y=int(9.5)
+print(y)
+print(type(y))
