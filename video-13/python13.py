0e71b6d287193ee71d1026559d92bf39687f4bf8
Shubhanshu Nishad <69467788+Shubhanshu-Nishad@users.noreply.github.com>
2020-10-03 20:28:32 +0530

    Add files via upload

---
+++ video-13/python13.py
@@ -0,0 +1,27 @@
+# if statement
+
+age = int(input('enter your age : '))
+# if age > 18 :
+#     print('you can drive')
+
+# 45<18
+# if age < 18 :
+#     print('you can not  drive')
+# else:
+#     print('you can drive')
+
+
+# 19<18
+if age < 18 :
+    print('you can not  drive')
+elif age == 20:
+    print("you can not drive because you are bad boy")
+elif age == 21:
+    print("you ")
+    # pass   if but if you for some reason have an if statement with no content, put in the pass statement to avoid getting an error.
+else:
+    print('you can drive')
+
+
+
+
