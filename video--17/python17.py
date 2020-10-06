c203f48b1cd58a5971a3021f480c15a76baf7e2c
Shubhanshu Nishad <69467788+Shubhanshu-Nishad@users.noreply.github.com>
2020-10-06 12:31:12 +0530

    Add files via upload

---
+++ video--17/python17.py
@@ -0,0 +1,28 @@
+# for loop 
+# i=2
+# print(' table of 2 ')
+# while i<=20:
+#     print(i)
+#     i= i+2
+
+# for i in [1,2,3,4]:
+#     print(i)
+
+# print(range(4)) # from 0  to n-1(here 4-1= 3)
+
+# print(range(500)) # from 0  to n-1
+
+# for i in range(4): #[0,1,2,3]
+#     print(i)
+
+# for i in range(1,4): #[1,2,3]
+#     print(i) 
+
+
+for i in range(2,4): #[2,3]
+    pass
+
+for i in range(1,11):
+    print(i*2)
+
+# print table from 2 to 20 using while loop & for loop 
