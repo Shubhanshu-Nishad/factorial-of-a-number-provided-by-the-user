ee94f88638f1ff0fce49e44293e1218b9c56a0ed
Shubhanshu Nishad <69467788+Shubhanshu-Nishad@users.noreply.github.com>
2020-10-09 10:23:43 +0530

    Add files via upload

---
+++ video-18/python18-2.py
@@ -0,0 +1,30 @@
+# list
+list1 = ['pen','copy','python','apple']
+# print(list1) # print the elements in a list
+
+# for i in list1:
+#     print('I have to buy ',i)
+# list1.append('mango') # it add elements in the last of list
+
+# print(list1[3]) # print the index elements
+# print(list1[1:4]) # print the index elements(n-1,4-1=3)
+# print(list1)
+# print(list1[-1]) # print the elements from the last in list
+# list1[3]= 'zoo' # replace the elements of specific index
+# print(len(list1)) # count the no of elements
+
+# list2 = ['gun','bullets']
+# # list2.extend(list1) # it add two list
+# print(list2)
+
+# list3 = list1 + list2
+# print(list3)
+# list1.remove('pen') # it removes the elements
+# list1.clear() # it empty the list
+# del list1
+# print(list1)
+
+fruits = ['apple', 'banana', 'cherry']
+
+fruits.reverse()
+print(fruits)
