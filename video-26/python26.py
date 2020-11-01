bb9229f824cb9a19895b83328c0b39cb5ebdefba
Shubhanshu Nishad <69467788+Shubhanshu-Nishad@users.noreply.github.com>
2020-11-01 21:27:38 +0530

    Add files via upload

---
+++ video-26/python26.py
@@ -0,0 +1,36 @@
+# swaping of two variables 
+
+x = 5 
+y = 10
+
+print("the value of x is ", x)
+print("the value of y is ", y)
+
+# method 1 Using third variables 
+
+# z = x 
+# x = y 
+# y =  z
+
+# method 2 without using third variables 
+# x= 5 , y= 10
+
+# x = x + y # 15 , 5 +10=15
+# y = x - y # 5 , 15 - 10 = 5
+# x = x - y # 10 , 15 -5 = 10
+
+# method 3 only in python 
+# x= 5 , y= 10
+y , x = x , y 
+
+a , b , c = 5 , 6, 7 
+
+# a = 5
+# b = 6 
+# c = 7
+ 
+
+
+
+print("the value of x is ", x)
+print("the value of y is ", y)
