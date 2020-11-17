ce568e2910a33ecc1cd77c7b4c486c77d8d5ca7c
Shubhanshu Nishad <69467788+Shubhanshu-Nishad@users.noreply.github.com>
2020-11-17 15:14:09 +0530

    Add files via upload

---
+++ video-31/python31.py
@@ -0,0 +1,15 @@
+# shorthand if 
+a= 100
+b = 500
+c = 2000
+
+# # if a>b:
+# #     print("a is greater than b")
+# # else:
+# #     print('b is greater than a')
+# check  greater number between two numbers
+# print("a is greater than b") if a>b else print('b is greater than a')
+
+# check  greater number between three numbers
+
+print("a is greater ") if a>b and a>c else print('b is greater') if b > c else print('c is greater ')
