0553991cc65b5d8cce6bf89a2bc038d64478d004
Shubhanshu Nishad <69467788+Shubhanshu-Nishad@users.noreply.github.com>
2020-12-17 15:40:36 +0530

    Add files via upload

---
+++ video-34/python34.py
@@ -0,0 +1,11 @@
+# f string
+a= int(input("enter numbers :"))
+b= int(input("enter numbers :"))
+c= a+b 
+print(f"the sum of {a} & {b} is {c} "  )#simple code 
+
+print("the sum of", a ,"&" ,b, "is", c) 
+
+val = 'Metaeducators'
+print(f"{val} is a portal for learning coding in mother language .")
+print(val ,"is a portal for learning coding in mother language .")  
