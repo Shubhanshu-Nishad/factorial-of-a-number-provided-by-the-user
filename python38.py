27fc2297599e6614bcbc25882d1b6e05f1441576
Shubhanshu Nishad <69467788+Shubhanshu-Nishad@users.noreply.github.com>
2021-01-10 09:41:00 +0530

    Add files via upload

---
+++ python38.py
@@ -0,0 +1,11 @@
+# random module 
+import random
+a = random.randint(1,6) # 0 <= a <=n 
+print(a)
+b = random.random() # float value from 0 < b < 1 
+c = random.random() *100 
+print(b)
+print(c)
+lst = ['metaeducators', 'shrish ',"hariom ", " manisha "]
+d = random.choice(lst)
+print(d)
